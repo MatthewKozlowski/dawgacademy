658d67c3604d9fe9257b6c8f77e303371197ca41
Matthew Kozlowski <44451986+MatthewKozlowski@users.noreply.github.com>
2021-04-16 14:45:10 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+'use strict'
+
+function signup(){
+    $('#signup').on('click', function(event){
+        $('.Logo').remove();
+        $('#pageContent').append(`
+            <div id="emailCapture">If you would like to be notified when we launch, please leave your email address here.
+            <br /><br />
+        <input type="text" id="emailAddress" size="40"/>
+        <input type="button" id="btnSubmit" value="Submit"/>
+        <div style="padding:10px;margin:10px;" id="emailResponse"></div>
+    </div>`);
+    })
+}
+
+function reload(){
+    $('#title').on('click', function(event){
+        location.reload();
+    })
+}
+
 $(function(){
-    console.log("")
+    console.log("Homepage Loaded")
+    signup();
+    reload();
 })
