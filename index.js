5fdcc104d75a0b5184ae58f5749217098fdd64cd
Matthew Kozlowski <44451986+MatthewKozlowski@users.noreply.github.com>
2021-04-26 14:33:29 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 'use strict'
 
 function signup(){
-    $('#signup').on('click', function(event){
+    $('#signupButton').on('click', function(event){
         $('.Logo').remove();
+        $('#pageContent').empty();
         $('#pageContent').append(`
             <div id="emailCapture">If you would like to be notified when we launch, please leave your email address here.
-            <br /><br />
-        <input type="text" id="emailAddress" size="40"/>
-        <input type="button" id="btnSubmit" value="Submit"/>
-        <div style="padding:10px;margin:10px;" id="emailResponse"></div>
-    </div>`);
+                <br /><br />
+                <input type="text" id="emailAddress" size="40"/>
+                <input type="button" id="btnSubmit" value="Submit"/>
+                <div style="padding:10px;margin:10px;" id="emailResponse"></div>
+             </div>`);
     })
 }
 
+function mission(){
+    $('#missionButton').on('click', function(event){
+        $('.Logo').remove();
+        $('#pageContent').empty();
+        $('#pageContent').append(`<h2 id="mission">Our Mission</h2>
+        `);
+    })
+}
 function reload(){
     $('#title').on('click', function(event){
         location.reload();
@@ -22,5 +31,6 @@ function reload(){
 $(function(){
     console.log("Homepage Loaded")
     signup();
+    mission();
     reload();
 })
